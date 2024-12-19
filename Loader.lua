44f21024300c73e66ccf131a7ec59cd4ce8a246b
Harvox <134923684+Harvox@users.noreply.github.com>
2024-12-19 04:50:22 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,6 +3,6 @@ local games = {
 
 if table.find(games, game.PlaceId) do 
 
-  else
-    
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Harvox/GlacierV2/refs/heads/main/Universal.lua"))()
 end  
