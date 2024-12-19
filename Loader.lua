70a678bc4a21d0444deab440babd19cbc8d01c3d
Harvox <134923684+Harvox@users.noreply.github.com>
2024-12-19 05:08:31 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -10,10 +10,10 @@ shared.version = nil
 local games = {
 }
 
-if table.find(games, game.PlaceId) do 
+if table.find(games, game.PlaceId) then 
 shared.version = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
 else
 shared.version = "Universal 2.0"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Harvox/GlacierV2/refs/heads/main/Universal.lua"))()
 end  
-notifyfunction("< GLACIERⱽ² 2.3 >", shared.version)
+notifyfunction("< GLACIERⱽ² 2.3 >", "Detected version/game: " .. shared.version)
