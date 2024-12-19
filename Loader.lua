6271f1dac00a6c5cdd8df139b70b83fa8c1903d3
Harvox <134923684+Harvox@users.noreply.github.com>
2024-12-19 05:07:22 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,11 @@
+getgenv().notifyfunction = function(title, text)
+game:GetService("StarterGui"):SetCore("SendNotification", {
+Title = title,
+Text = text,
+Button1 = "Alright!",
+})
+end
+
 shared.version = nil
 local games = {
 }
@@ -8,3 +16,4 @@ else
 shared.version = "Universal 2.0"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Harvox/GlacierV2/refs/heads/main/Universal.lua"))()
 end  
+notifyfunction("< GLACIERⱽ² 2.3 >", shared.version)
