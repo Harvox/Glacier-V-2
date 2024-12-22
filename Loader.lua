4786cfcc5d1f3aa8965d5a5894475860a83ea7db
Harvox <134923684+Harvox@users.noreply.github.com>
2024-12-22 10:10:31 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -16,5 +16,8 @@ else
 shared.version = "Universal 2.0"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Harvox/GlacierV2/refs/heads/main/Universal.lua"))()
 end  
+if string.find(string.lower(tostring(identifyexecutor())), "synapse z") then 
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/liam0999/Drawing2/refs/heads/main/Drawing2.lua"))();
+end
 notifyfunction("< GLACIERⱽ² 2.3 >", "Detected version/game: " .. shared.version)
 loadstring(game:HttpGet("https://pastebin.com/raw/a2CV7cAg"))() -- Don't remove or it'll break the script!
